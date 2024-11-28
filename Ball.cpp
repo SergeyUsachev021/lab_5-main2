9f773dea10943dd45d00b6f42b03cecf484ec185
Sergey <squ2@tpu.ru>
2024-11-28 17:02:28 +0700

    wqerwq

--- Ball.cpp
+++ Ball.cpp
@@ -1,20 +1,23 @@
-#include "Ball.hpp"  
-#include <cmath>     
+#include "Ball.hpp"
+#include <cmath>
 
-// Конструктор класса Ball, инициализирует объект с заданными параметрами mass radius set
-Ball::Ball(const Velocity velocity, const Point center, double radius, const Color color, bool isCollidable): 
-     velocity_(velocity), center_(center), radius_(radius), color_(color), isCollidable_(isCollidable) {}
+// Конструктор класса Ball, инициализирует объект с заданными параметрами mass
+// radius set
+Ball::Ball(const Velocity velocity, const Point center, double radius,
+           const Color color, bool isCollidable)
+    : velocity_(velocity), center_(center), radius_(radius), color_(color),
+      isCollidable_(isCollidable) {}
 
 // Задает скорость объекта
 // @param velocity новое значение скорости
 void Ball::setVelocity(const Velocity& velocity) {
-    this->velocity_ = velocity;  // Устанавливает новую скорость для объекта
+    this->velocity_ = velocity; // Устанавливает новую скорость для объекта
 }
 
 // @return скорость объекта
 Velocity Ball::getVelocity() const {
-    // TODO: место для доработки (может быть добавлена дополнительная логика)
-    return velocity_;  // Возвращает текущую скорость объекта
+    // TODO: место для доработки (моqжет быть добавлена дополнительная логика)
+    return velocity_; // Возвращает текущую скорость объекта
 }
 
 // @brief Выполняет отрисовку объекта
@@ -24,25 +27,27 @@ Velocity Ball::getVelocity() const {
 // Рисование выполняется путем вызова painter.draw(...)
 // @param painter контекст отрисовки
 void Ball::draw(Painter& painter) const {
-    painter.draw(center_, radius_, color_);  // Вызывает метод draw у объекта painter для отрисовки шара
+    painter.draw(
+        center_, radius_,
+        color_); // Вызывает метод draw у объекта painter для отрисовки шара
 }
 
 // Задает координаты центра объекта
 // @param center новый центр объекта
 void Ball::setCenter(const Point& center) {
-    this->center_ = center;  // Устанавливает новый центр для объекта
+    this->center_ = center; // Устанавливает новый центр для объекта
 }
 
 // @return центр объекта
 Point Ball::getCenter() const {
-    return center_;  // Возвращает текущие координаты центра объекта
+    return center_; // Возвращает текущие координаты центра объекта
 }
 
 // @brief Возвращает радиус объекта
 // @details обратите внимание, что метод setRadius()
 // не требуется (радиус задается только при создании)
 double Ball::getRadius() const {
-    return radius_;  // Возвращает радиус шара
+    return radius_; // Возвращает радиус шара
 }
 
 // @brief Возвращает массу объекта
@@ -51,13 +56,15 @@ double Ball::getRadius() const {
 // плотностью. В этом случае масса в условных единицах
 // эквивалентна объему: PI * radius^3 * 4. / 3.
 double Ball::getMass() const {
-    return M_PI * pow(radius_, 3) * 4. / 3.;  // Вычисляет и возвращает массу шара по формуле объема сферы
+    return M_PI * pow(radius_, 3) * 4. /
+           3.; // Вычисляет и возвращает массу шара по формуле объема сферы
 }
 
 Color Ball::getColor() const {
-    return color_;  // Возвращает цвет шара
+    return color_; // Возвращает цвет шара
 }
 
-bool Ball::ifCollidable() const{
-    return isCollidable_;  // Возвращает true, если шар может сталкиваться с другими объектами
+bool Ball::ifCollidable() const {
+    return isCollidable_; // Возвращает true, если шар может сталкиваться с
+                          // другими объектами
 }
